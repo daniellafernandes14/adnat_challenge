e02044e76f710456d75eb1fe48e38359fa4b9d30
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-12-01 20:24:51 +0000

    Updated Edit buttons and Create organisation form on index page

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,17 +1,33 @@
 class OrganisationsController < ApplicationController
 
   def create
+    @organisation = Organisation.new(organisation_params)
+    @organisation.save
+    render 'create'
   end
 
   def update
+    @organisation = Organisation.find(params[:id])
+    @organisation.update(organisation_params)
+    # redirect_to
   end
 
   def edit
+    @organisation = Organisation.find(params[:id])
   end
 
   def show
+    @organisation = Organisation.new
   end
 
   def index
+    @organisations = Organisation.all
+    @organisation = Organisation.new
+  end
+
+  private
+
+  def organisation_params
+    params.require(:organisation).permit(:name, :hourly_rate)
   end
 end
