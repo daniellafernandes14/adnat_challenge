183c3b61753c7d4d72658c2887b4f3b84025c70b
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-12-01 15:22:50 +0000

    added the views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'organisations#index'
-  resources :organisations, only: %i[create update edit show index]
-  resources :shifts, only: %i[create new index]
+  resources :organisations, only: %i[create update edit show index] do
+   resources :shifts, only: %i[create new index]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_01_145644) do
+ActiveRecord::Schema.define(version: 2021_12_01_150659) do
 
   create_table "organisations", force: :cascade do |t|
     t.string "name"
     t.integer "hourly_rate"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "shifts", force: :cascade do |t|
+    t.datetime "start"
+    t.datetime "finish"
+    t.integer "break_length"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_shifts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +44,5 @@ ActiveRecord::Schema.define(version: 2021_12_01_145644) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "shifts", "users"
 end
