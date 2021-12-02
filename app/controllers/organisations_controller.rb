6f06980c861bea87ccbb8418e11f715f9dc574ea
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-12-02 13:18:54 +0000

    Updated Edit page with form and delete button

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -4,6 +4,8 @@ class OrganisationsController < ApplicationController
     @organisation = Organisation.new(organisation_params)
     @organisation.save
     redirect_to organisations_path
+    # @organisation = Organisation.find(@organisation_id)
+    @organisation = current_user.organisation_id
   end
 
   def update
@@ -23,6 +25,12 @@ class OrganisationsController < ApplicationController
   def index
     @organisations = Organisation.all
     @organisation = Organisation.new
+  end
+
+  def destroy
+    @organisation = Organisation.find(params[:id])
+    @organisation.destroy
+    redirect_to organisations_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'organisations#index'
-  resources :organisations, only: %i[create update edit show index] do
+  resources :organisations, only: %i[destroy create update edit show index] do
    resources :shifts, only: %i[create new index]
   end
 end
