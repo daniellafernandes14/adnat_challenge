342807c912f91183770dc5bb47ff6737d34f93f1
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-12-03 11:43:05 +0000

    Added shift table and form to fill out, trying to get it to save

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,10 +1,27 @@
 class ShiftsController < ApplicationController
   def create
+    @shift = Shift.new(shift_params)
+    @organisation = Organisation.find(params[:organisation_id])
+    @shift.user_id = current_user
+    if @shift.save
+      redirect_to organisation_shifts_path(@organisation)
+    else
+      render :new
+    end
   end
 
   def new
   end
 
   def index
+    @shifts = Shift.all
+    @shift = Shift.new
+    @organisation = Organisation.find(params[:organisation_id])
+  end
+
+  private
+
+  def shift_params
+    params.require(:shift).permit(:start, :finish, :break_length)
   end
 end
